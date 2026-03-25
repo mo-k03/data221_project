3f65431e535158d26132cfd8c49574da9d807b79
jybpan <junyubpan@gmail.com>
2026-03-25 09:37:41 -0600

    Split preprocessing, and actual model into two separate files

--- randomForestModel.py
+++ randomForestModel.py
@@ -2,48 +2,4 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
-def has_basement(sqft_basement):
-    if sqft_basement != 0:
-        return 1
-    else:
-        return 0
-
-def yearsSinceRenovation(yr_renovated):
-    if yr_renovated != 0:
-        return int(YEAR - yr_renovated)
-    else:
-        return int(YEAR - features['yr_built'].mean())
-
-def removeOutliers(df, column):
-    Q1 = df[column].quantile(0.25)
-    Q3 = df[column].quantile(0.75)
-
-    IQR = Q3-Q1
-
-    lower = Q1-(1.5*IQR)
-    upper = Q3+(1.5*IQR)
-    
-    return df[(dataframe[column] >= lower) & (df[column] <= upper)]
-
-YEAR = 2015
-
-dataframe = pd.read_csv("kc_house_data.csv")
-
-dataframe = dataframe.drop(["id", "date", "zipcode", "sqft_living15", "sqft_lot15"], axis=1) # Drop the redundant variables
-
-labels = dataframe['price']
-labels = removeOutliers(labels, 'price')
-labels = labels[labels.index]
-
-features = dataframe.drop('price',axis=1)
-
-features['years_since_renovation'] = features['yr_renovated'].apply(yearsSinceRenovation) # Turn year renovated to the years since renovation,
-features = features.drop(['yr_built', 'yr_renovated'],axis=1)                             # which is far more useful for models than having "year renovated"
-
-features['has_basement'] = features['sqft_basement'].apply(has_basement)
-features = features.drop('sqft_basement', axis=1)
-
 xTrain, xTest, yTrain, yTest = train_test_split(features, labels, test_size=0.2, random_state=221)
-
-features
-index
