3cce7d3182fa64b6d504d511457fdfe06e2ee397
jybpan <junyubpan@gmail.com>
2026-03-27 16:00:39 -0600

    Fix up minor file and dataframe bugs, reformat performance metrics

---
+++ models/random_forest/random_forest.py
@@ -0,0 +1,37 @@
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+
+import pandas as pd
+
+from utils.common import dataCleaning
+
+# preprocesses data
+dataCleaning("data/kc_house_data.csv")
+
+# data loading
+dataframe = pd.read_csv("data/kc_house_data_cleaned.csv")
+
+features = dataframe.drop('price', axis=1)
+labels = dataframe['price']
+
+xTrain, xTest, yTrain, yTest = train_test_split(features, labels, test_size=0.20, random_state=221)
+
+model = RandomForestRegressor(
+                n_estimators=300, max_depth=20, random_state=221,
+                min_samples_leaf=2, min_samples_split=5, max_features=0.70
+                )
+model.fit(xTrain, yTrain)
+
+predictedTrain = model.predict(xTrain)
+predictedTest = model.predict(xTest)
+
+r2TrainingScore = r2_score(yTrain, predictedTrain)
+r2TestingScore = r2_score(yTest, predictedTest)
+mae = mean_absolute_error(yTest, predictedTest)
+mse = mean_squared_error(yTest, predictedTest)
+
+print(f"R^2 Training Score: {r2TrainingScore:.2f}\nR^2 Testing Score: {r2TestingScore:.2f}\nMean Absolute Error: {mae:.2f}\nMean Squared Error: {mse:.2f}")
--- utils/common.py
+++ utils/common.py
@@ -1,18 +1,14 @@
 import pandas as pd 
 
-from sklearn.model_selection import train_test_split
-
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
-
-
 def hasBasement(sqft_basement):
     if sqft_basement != 0:
         return 1
     else:
         return 0
 
-def yearsSinceRenovation(yr_renovated):
+def yearsSinceRenovation(yr_renovated, features):
+    YEAR = 2015
+
     meanYearBuilt = features['yr_built'].mean()
     
     if yr_renovated != 0:
@@ -31,20 +27,14 @@ def removeOutliers(dataframe, column):
     return dataframe[(dataframe[column] >= lower) & (dataframe[column] <= upper)]
 
 def dataCleaning(FILE_NAME):
-    YEAR = 2015
-
     dataframe = pd.read_csv(FILE_NAME)
 
     dataframe = dataframe.drop(["id", "date", "zipcode"], axis=1) # Drop the redundant variables
     dataframe = removeOutliers(dataframe, "price") # Remove the price outliers from the dataframe using IQR
 
-    labels = dataframe['price']
-    features = dataframe.drop('price',axis=1)
+    dataframe['years_since_renovation'] = dataframe['yr_renovated'].apply(lambda value: yearsSinceRenovation(value, dataframe)) # Turn year renovated to the years since renovation,
+    dataframe['has_basement'] = dataframe['sqft_basement'].apply(hasBasement)                            # which is far more useful for models than having "year renovated"
 
-    features['years_since_renovation'] = features['yr_renovated'].apply(yearsSinceRenovation) # Turn year renovated to the years since renovation,
-    features = features.drop(['yr_built', 'yr_renovated'],axis=1)                             # which is far more useful for models than having "year renovated"
-
-    features['has_basement'] = features['sqft_basement'].apply(hasBasement) #Turn the sqft of basements into has_basement,
-    features = features.drop('sqft_basement', axis=1)                        #again, far more useful than its square feet.
+    dataframe = dataframe.drop(['yr_built', 'yr_renovated', 'sqft_basement'], axis=1) #Turn the sqft of basements into has_basement, again, far more useful than its square feet.
 
     dataframe.to_csv('data/kc_house_data_cleaned.csv', index=False)
