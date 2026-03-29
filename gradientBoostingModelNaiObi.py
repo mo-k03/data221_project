c27f22c2fc64d034e8ed0557edf156f44a97d017
naiyomiobi <chibu.obi@ucalgary.ca>
2026-03-29 17:24:21 -0600

    loaded our data file

--- gradientBoostingModelNaiObi.py
+++ gradientBoostingModelNaiObi.py
@@ -5,3 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
+df = pd.read_csv("kc_house_data.csv")
+
+
+
