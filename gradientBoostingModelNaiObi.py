f23b49956c1cd7757c8f76556b72f30fd3694a1e
naiyomiobi <chibu.obi@ucalgary.ca>
2026-03-29 17:52:47 -0600

    train test split

--- gradientBoostingModelNaiObi.py
+++ gradientBoostingModelNaiObi.py
@@ -19,3 +19,7 @@ for col in df.select_dtypes(include=['object']).columns:
 
 x = df.drop("price", axis=1)
 y = df["price"]
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size= 0.2, random_state= 221
+)
