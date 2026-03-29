37d21ed02db2329db2c5ea2fa85932e71a48a57c
naiyomiobi <chibu.obi@ucalgary.ca>
2026-03-29 17:02:44 -0600

    added all my imports

--- gradientBoostingModelNaiObi.py
+++ gradientBoostingModelNaiObi.py
@@ -1,1 +1,7 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
