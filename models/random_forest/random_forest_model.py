1079426a1f0950ff6ad7399c93d8eb6bc1b61061
jybpan <junyubpan@gmail.com>
2026-03-29 22:19:27 -0600

    Add explanation to Random Forest parameters

--- models/random_forest/random_forest_model.py
+++ models/random_forest/random_forest_model.py
@@ -19,13 +19,30 @@ def randomForestModel():
     features = dataframe.drop('price', axis=1)
     labels = dataframe['price']
 
+    print("Splitting Dataset.....")
+
     xTrain, xTest, yTrain, yTest = train_test_split(features, labels, test_size=0.20, random_state=221)
+
+    print("Dataset successfully split.")
 
     model = RandomForestRegressor(
                     n_estimators=300, max_depth=20, random_state=221,
                     min_samples_leaf=2, min_samples_split=5, max_features=0.70
-                    )
+                    ) 
+
+    #N Estimators = amount of decision trees in the forest. More trees = more consistent predictions.
+    #Max Depth = How deep each tree is allowed to grow, each depth level adds another split.
+    #Min Samples Leaf = Minimum amount of training samples for each leaf (where predictions are made). Prevents a tree from making very specific splits for one singular data point.
+    #Min Samples Split = Minimum amount of samples needed before a node can be split. Prevents tiny splits from happening.
+    #Max Features = Percentage of the features a tree should consider. (i.e 70% -> tree only considers 70% of features). This is the random in random forest.
+    
+    #In general, these tunes are here to soften the blow of an already overfitting-prone model.
+
+    print("Fitting Model.....")
+
     model.fit(xTrain, yTrain)
+
+    print("Model Fitted.")
 
     predictedTrain = model.predict(xTrain)
     predictedTest = model.predict(xTest)
@@ -35,10 +52,11 @@ def randomForestModel():
     mae = mean_absolute_error(yTest, predictedTest)
     mse = mean_squared_error(yTest, predictedTest)
 
-    print(f"R^2 Training Score: {r2TrainingScore:.2f}\nR^2 Testing Score: {r2TestingScore:.2f}\nMean Absolute Error: {mae:.2f}\nMean Squared Error: {mse:.2f}")
+    print(f"\n\nR^2 Training Score: {r2TrainingScore:.2f}\nR^2 Testing Score: {r2TestingScore:.2f}\nMean Absolute Error: {mae:.2f}\nMean Squared Error: {mse:.2f}")
 
     # there is slight overfitting within the model, which is seen in the difference between the R^2 of the training, and testing score.
     # this amount of overfitting is generally normal within random forest models due to its complexity and amount of parameters,
     # i have attempted to tune the parameters to minimize overfitting and reduce the MAE and MSE as much as possible.
 
 randomForestModel()
+
